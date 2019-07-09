da99703d82f3f987fc6a3cbfc26d240f1f6b43b4
RamyaCGAithabattula <52093645+RamyaCGAithabattula@users.noreply.github.com>
2019-07-09 14:25:01 +0530

    Add files via upload

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,17 @@
 ﻿import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
-import { AddEmployeeComponent } from './app.addemployee';
+import {HttpClientModule} from '@angular/common/http';
+import { AddEmployeeComponent } from './addemployeecomponent';
+import { ShowEmployeeComponent} from './showemployeecomponent';
 import {FormsModule} from '@angular/forms';
 @NgModule({
     imports: [
-        BrowserModule,FormsModule
+        BrowserModule,HttpClientModule,FormsModule
         
     ],
     declarations: [
-        AppComponent,AddEmployeeComponent
+        AppComponent,AddEmployeeComponent,ShowEmployeeComponent
 		],
     providers: [ ],
     bootstrap: [AppComponent]
